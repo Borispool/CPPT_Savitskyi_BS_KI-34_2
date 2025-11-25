0b8f36d3d4055cdaa260f3670a43a7bef0a36010
Borys Savitskyi <borys.savitskyi.ki.2023@lpnu.ua>
2025-11-25 20:53:34 +0200

    Added Lab6 folder manually

---
+++ Lab6_Java/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI34.Savitskyi.Lab6","l":"Safe"},{"p":"KI34.Savitskyi.Lab6","l":"SafeDriver"}];updateSearchResults();
