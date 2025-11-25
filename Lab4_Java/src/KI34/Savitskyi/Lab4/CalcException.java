ba0d6592debff4692773e5507f7571d7257846ed
Borys Savitskyi <borys.savitskyi.ki.2023@lpnu.ua>
2025-11-25 20:49:57 +0200

    Added Lab4 folder manually

---
+++ Lab4_Java/src/KI34/Savitskyi/Lab4/CalcException.java
@@ -0,0 +1,13 @@
+package KI34.Savitskyi.Lab4;
+
+public class CalcException extends ArithmeticException {
+
+    public CalcException() {
+        super();
+    }
+
+    public CalcException(String message) {
+        super(message);
+    }
+
+}
